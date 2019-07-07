cc61236d9f5efd0b8d4144d6f217a6f26a1ba2ab
Debaditya Chatterjee <dchatterjee89@outlook.com>
2019-07-07 19:33:44 +0530

    Add project files.

---
+++ TestAppAzure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestAppAzure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
